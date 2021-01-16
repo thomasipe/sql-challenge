93a75a7ae68169bf4efa01d292678fa5f5505545
Thomas Sipe <tsipe@Toms-MacBook-Pro.local>
2021-01-15 22:16:00 -0500

    first four queries complete

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -20,12 +20,41 @@ order by e.emp_no
 
 
 -- 2. List first name, last name, and hire date for employees who were hired in 1986.
+select first_name
+	, 	last_name
+	, 	hire_date
+	
+from employees
+where right(hire_date, 4) = '1986'
+
 
 
 -- 3, List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT dm.dept_no
+	,	dept_name
+	,	dm.emp_no
+	, 	last_name
+	,	first_name
+
+FROM	dept_manager dm
+INNER JOIN departments d ON
+	dm.dept_no = d.dept_no
+INNER JOIN employees e ON
+	dm.emp_no = e.emp_no
 
 
 -- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT de.emp_no
+	,	first_name 
+	,	last_name
+	,	dept_name
+
+FROM	dept_emp de
+INNER JOIN departments d ON
+	de.dept_no = d.dept_no
+INNER JOIN employees e ON
+	de.emp_no = e.emp_no
+
 
 
 -- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
