a16b130d5f513b0c966754343a3480aa3add8db0
Thomas Sipe <tsipe@Toms-MacBook-Pro.local>
2021-01-15 22:38:05 -0500

    all queries complete

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -82,7 +82,24 @@ WHERE dept_name = 'Sales'
 	
 
 -- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT de.emp_no
+	,	first_name 
+	,	last_name
+	,	dept_name
 
+FROM	dept_emp de
+INNER JOIN departments d ON
+	de.dept_no = d.dept_no
+INNER JOIN employees e ON
+	de.emp_no = e.emp_no
+
+WHERE dept_name IN ('Sales', 'Development')
+	
 
 -- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, count(*) as "last_name_count"
+FROM employees
+GROUP BY last_name
+ORDER BY count(*) desc
+
 
